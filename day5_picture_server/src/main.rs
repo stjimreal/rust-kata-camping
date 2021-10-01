a6c43c90112bd9aaa0caf8b2cc8c33caa9857e00
jimlau <stjimreal@gmail.com>
2021-10-02 03:18:00 +0800

    add image processing part

--- day5_picture_server/src/main.rs
+++ day5_picture_server/src/main.rs
@@ -1,13 +1,24 @@
 /*
  * @Date: 2021-09-30 22:28:50
  * @LastEditors: LIULIJING
- * @LastEditTime: 2021-10-02 02:33:00
+ * @LastEditTime: 2021-10-02 03:13:09
  */
 
-use axum::{extract::Path, handler::get, http::StatusCode, Router};
-use percent_encoding::percent_decode_str;
+use anyhow::Result;
+use axum::{AddExtensionLayer, Router, extract::{Extension, Path}, handler::get, http::{HeaderMap, HeaderValue, StatusCode}};
+use bytes::Bytes;
+use lru::LruCache;
+use percent_encoding::{percent_decode_str, percent_encode, NON_ALPHANUMERIC};
 use serde::Deserialize;
-use std::convert::TryInto;
+use std::{
+    collections::hash_map::DefaultHasher,
+    convert::TryInto,
+    hash::{Hash, Hasher},
+    sync::Arc,
+};
+use tokio::sync::Mutex;
+use tower::ServiceBuilder;
+use tracing::{info, instrument};
 
 // 引入 protobuf 生成的代码，我们暂且不用太关心他们
 mod pb;
@@ -20,31 +31,89 @@ struct Params {
     spec: String,
     url: String
 }
+type Cache = Arc<Mutex<LruCache<u64, Bytes>>>;
 
-async fn generate(Path(Params {spec, url}): Path<Params>) -> Result<String, StatusCode> {
+#[instrument(level = "info", skip(cache))]
+async fn retrieve_image(url: &str, cache: Cache) -> Result<Bytes> {
+    let mut hasher = DefaultHasher::new();
+    url.hash(&mut hasher);
+    let key = hasher.finish();
+
+    let g = &mut cache.lock().await;
+    let data = match g.get(&key) {
+        Some(v) => {
+            info!("Match cache {}", key);
+            v.to_owned()
+        }
+        None => {
+            info!("Retrieve url");
+            let resp = reqwest::get(url).await?;
+            let data = resp.bytes().await?;
+            g.put(key, data.clone());
+            data
+        }
+    };
+
+    Ok(data)
+}
+
+async fn generate(
+    Path(Params {spec, url}): Path<Params>,
+    Extension(cache): Extension<Cache>,    
+) -> Result<(HeaderMap, Vec<u8>), StatusCode> {
     let url = percent_decode_str(&url).decode_utf8_lossy();
     let spec: ImageSpec = spec
         .as_str()
         .try_into()
         .map_err(|_|StatusCode::BAD_REQUEST)?;
-    Ok(format!("url: {}\n spec: {:#?}", url, spec))
+    let data = retrieve_image(&url, cache)
+        .await
+        .map_err(|_| StatusCode::BAD_REQUEST)?;
+
+    // TODO: 处理图片
+
+    let mut headers = HeaderMap::new();
+
+    headers.insert("content-type", HeaderValue::from_static("image/jpeg"));
+    Ok((headers, data.to_vec()))
 }
 
 #[tokio::main]
 async fn main() {
     // 初始化 tracing
     tracing_subscriber::fmt::init();
+    let cache:Cache = Arc::new(Mutex::new(LruCache::new(1024)));
 
     // 构建路由
     let app = Router::new()
         // `GET /image` 会执行 generate 函数，并把 spec 和 url 传递过去
-        .route("/image/:spec/:url", get(generate));
+        .route("/image/:spec/:url", get(generate))
+        .layer(
+            ServiceBuilder::new()
+                .layer(AddExtensionLayer::new(cache))
+                .into_inner(),
+        );
     
     // 运行 web 服务器
     let addr = "127.0.0.1:3000".parse().unwrap();
+
+    print_test_url("https://images.pexels.com/photos/1562477/pexels-photo-1562477.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260");
+
     tracing::debug!("listening on {}", addr);
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
         .await
         .unwrap();
+}
+
+// 调试辅助函数
+fn print_test_url(url: &str) {
+    use std::borrow::Borrow;
+    let spec1 = Spec::new_resize(500, 800, resize::SampleFilter::CatmullRom);
+    let spec2 = Spec::new_watermark(20, 20);
+    let spec3 = Spec::new_filter(filter::Filter::Marine);
+    let image_spec = ImageSpec::new(vec![spec1, spec2, spec3]);
+    let s: String = image_spec.borrow().into();
+    let test_image = percent_encode(url.as_bytes(), NON_ALPHANUMERIC).to_string();
+    println!("test url: http://localhost:3000/image/{}/{}", s, test_image);
 }
